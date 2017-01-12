a4f0b7ba0beceba99a7e52e2f62baf0b6f619beb
Pradeep Tewani <pradeep@sysplay.in>
2017-01-13 00:44:12 +0530

    Did the further code clean up and added the omap-serial-org.c from which the char serial driver was derived

--- Uart/Code/my_serial.h
+++ Uart/Code/my_serial.h
@@ -1,35 +1,8 @@
 #ifndef MY_SERIAL_H
 #define MY_SERIAL_H
 
-struct uart_omap_dma {
-	u8			uart_dma_tx;
-	u8			uart_dma_rx;
-	int			rx_dma_channel;
-	int			tx_dma_channel;
-	dma_addr_t		rx_buf_dma_phys;
-	dma_addr_t		tx_buf_dma_phys;
-	unsigned int		uart_base;
-	/*
-	 * Buffer for rx dma.It is not required for tx because the buffer
-	 * comes from port structure.
-	 */
-	unsigned char		*rx_buf;
-	unsigned int		prev_rx_dma_pos;
-	int			tx_buf_size;
-	int			tx_dma_used;
-	int			rx_dma_used;
-	spinlock_t		tx_lock;
-	spinlock_t		rx_lock;
-	/* timer to poll activity on rx dma */
-	struct timer_list	rx_timer;
-	unsigned int		rx_buf_size;
-	unsigned int		rx_poll_rate;
-	unsigned int		rx_timeout;
-};
-
 struct uart_omap_port {
 	struct uart_port	port;
-	struct uart_omap_dma	uart_dma;
 	struct device		*dev;
 
 	unsigned char		ier;
@@ -43,40 +16,23 @@ struct uart_omap_port {
 	unsigned char		scr;
 	unsigned char		wer;
 
-	int			use_dma;
 	/*
 	 * Some bits in registers are cleared on a read, so they must
 	 * be saved whenever the register is read but the bits will not
 	 * be immediately processed.
 	 */
-	unsigned int		lsr_break_flag;
-	unsigned char		msr_saved_flags;
 	char			name[20];
-	unsigned long		port_activity;
-	int			context_loss_cnt;
-	u32			errata;
 	u8			wakeups_enabled;
 	u32			features;
 
-	int			DTR_gpio;
-	int			DTR_inverted;
-	int			DTR_active;
-
-	struct serial_rs485	rs485;
-	int			rts_gpio;
-
-	struct pm_qos_request	pm_qos_request;
 	u32			latency;
 	u32			calc_latency;
-	struct work_struct	qos_work;
-	bool			is_suspending;
 	char *rx_buff;
 	size_t rx_size;
 	char *tx_buff;
 	size_t tx_size;
 };
 
-//void transmit_chars(struct uart_omap_port *up, char ch);
 int serial_read(struct uart_omap_port *up, size_t len);
 int fcd_init(struct uart_omap_port *up);
 void fcd_exit(void);
